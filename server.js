fde95edf1a3b3cd315b4eb65ad368203b252e383
xovest <79008917+xovest@users.noreply.github.com>
2021-10-24 16:35:37 +0300

    damn that backend beginning

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
+
+const posts = [
+  {
+    username: 'Ryan',
+    title: 'Post 1'
+  },
+  {
+    username: 'Walter',
+    title: 'Post 2'
+  }
+];
+
+app.get('/post', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name));
+
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  })
+}
+
+app.listen(3000);
